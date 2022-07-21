122cdc282978bdcdc476f586092220195a18660b
Zail <zzzzail@163.com>
2022-07-21 20:14:29 +0800

    1

--- java/jdk8/src/main/java/com/dsa/leetcode/jzof/P65.java
+++ java/jdk8/src/main/java/com/dsa/leetcode/jzof/P65.java
@@ -14,10 +14,10 @@ public class P65 {
     }
     
     public int add(int a, int b) {
-        while (b != 0) {           // 当进位为 0 时跳出
-            int c = (a & b) << 1;  // c = 进位
-            a ^= b;                // a = 非进位和
-            b = c;                 // b = 进位
+        while (b != 0) {
+            int c = (a & b) << 1; // 进位
+            a = a ^ b;            // 无进位和
+            b = c;
         }
         return a;
     }

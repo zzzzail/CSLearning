40a2402c02f5730ac48bc8f3876d0df5db915ce6
Zail <zzzzail@163.com>
2022-07-31 20:40:59 +0800

    写完了

--- python/spider/spider01.py
+++ python/spider/spider01.py
@@ -1,85 +1,70 @@
+import re
+import requests
 from bs4 import BeautifulSoup
 from urllib import parse
-import requests
 
 
 def first_step(word):
-    url = 'http://bncweb.lancs.ac.uk/cgi-binbncXML/processQuery.pl'
-    _word = "+".join(word.split(' '))
+    url = 'http://bncweb.lancs.ac.uk/cgi-binbncXML/processQuery.pl?chunk=1&queryType=CQL&qMode=Simple+query+%28ignore+case%29&inst=50&max=INIT&qname=INIT&thMode=INIT&thin=0&qtype=0&view=list&phon=0&theAction=Start+Query&urlTest=yes'
+    # _word = "+".join(word.split(' '))
     qs_params = {
-        'theData': _word,
-        'chunk': 1,
-        'queryType': 'CQL',
-        'qMode': 'Simple+query+%28ignore+case%29',
-        'inst': 50,
-        'max': 'INIT',
-        'qname': 'INIT',
-        'thMode': 'INIT',
-        'thin': 0,
-        'qtype': 0,
-        'view': 'list',
-        'phon': 0,
-        'theAction': 'Start+Query',
-        'urlTest': 'yes',
+        'theData': word,
     }
     # 这里不应该是 encode！因该是解析 querystring
     qs = parse.urlencode(qs_params)
-    url = url + '?' + qs
-    print(url)
+    url = url + '&' + qs
+    # print(url)
     headers = {
         'Authorization': 'Basic UmhlYTEyMzpEaW5ncnVpNTIw',
     }
     response = requests.get(url, headers=headers)
-    print(response.text)
-
-
-def second_step():
-    url = 'http://bncweb.lancs.ac.uk/cgi-binbncXML/dlogs.pl'
-    qs_params = {
-        'selected': 'Distribution',
-        'inst': 50,
-        'max': 66,
-        'chunk': 1,
-        'qname': 'Rhea123_1657765571',
-        'queryID': 'Rhea123_1657765571',
-        'thMode': 'M3259%231366%23no_subcorpus%23%23',
-        'thin': 0,
-        'theData': '%5Bword%3D%22take%22%25c%5D+%5Bword%3D%22place%22%25c%5D',
-        'warned': 0,
-        'numOfSolutions': 3259,
-        'numOfFiles': '',
-        'numOfSpeakers': 138,
-        'view': list,
-        'dbname': '',
-        'SQL': '',
-        'letter': '',
-        'phon': 0,
-        'tag': '',
-        'ot': '',
-        'display': 'word',
-        'exclude': '',
-        'program': 'search',
-        'run': '',
-        'theID': 'Rhea123_1657765571',
-        'urlTest': 'yes',
+    # print(response.text)
+    soup = BeautifulSoup(response.text, features="html.parser")
+    return {
+        'inst': soup.select('input[name=inst]')[0]['value'],
+        'max': soup.select('input[name=max]')[0]['value'],
+        'chunk': soup.select('input[name=chunk]')[0]['value'],
+        'thMode': soup.select('input[name=thMode]')[0]['value'],
+        'theData': soup.select('input[name=theData]')[0]['value'],
+        'numOfSolutions': soup.select('input[name=numOfSolutions]')[0]['value'],
+        'numOfSpeakers': soup.select('input[name=numOfSpeakers]')[0]['value'],
     }
+
+
+def second_step(qs_param):
+    url = 'http://bncweb.lancs.ac.uk/cgi-binbncXML/dlogs.pl?selected=Distribution&qname=Rhea123_1657765571&queryID=Rhea123_1657765571&thin=0&warned=0&numOfFiles=&view=list&dbname=&SQL=&letter=&phon=0&tag=&ot=&display=word&exclude=&program=search&run=&theID=Rhea123_1657765571&urlTest=yes'
+    qs = parse.urlencode(qs_param)
+    url = url + '&' + qs
+    # print(url)
     headers = {
         'Authorization': 'Basic UmhlYTEyMzpEaW5ncnVpNTIw',
     }
-    pass
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, features="html.parser")
+    sc = soup.find('td', text=re.compile('Spoken conversation')).parent.select('td')[4].text
+    osm = soup.find('td', text=re.compile('Other spoken material')).parent.select('td')[4].text
+    fav = soup.find('td', text=re.compile('Fiction and verse')).parent.select('td')[4].text
+    opwm = soup.find('td', text=re.compile('Other published written material')).parent.select('td')[4].text
+    n = soup.find('td', text=re.compile('Newspapers')).parent.select('td')[4].text
+    uwm = soup.find('td', text=re.compile('Unpublished written material')).parent.select('td')[4].text
+    napab = soup.find('td', text=re.compile('Non-academic prose and biography')).parent.select('td')[4].text
+    ap = soup.find('td', text=re.compile('Academic prose')).parent.select('td')[4].text
+    print(sc, ',', osm, ',', fav, ',', opwm, ',', n, ',', uwm, ',', napab, ',', ap)
 
 
-def do_spider():
-    pass
+def do_spider(word):
+    qs_param = first_step(word)
+    second_step(qs_param)
 
 
+# 这是程序开始的地方
 if __name__ == '__main__':
-    first_step('take place')
-    pass
-    print("爬虫开始")
     words = [
         'take place',
         'word',
     ]
     for word in words:
-        print(word)
+        try:
+            do_spider(word)
+        except Exception:
+            print('')

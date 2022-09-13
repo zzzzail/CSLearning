95d60cc75d526f00b72d34203caa4a855262207d
zhangxq <zzzzail@163.com>
2022-09-13 09:12:40 +0800

    1

--- java/jdk8/src/main/java/com/java/designpattern/command/Main.java
+++ java/jdk8/src/main/java/com/java/designpattern/command/Main.java
@@ -7,12 +7,9 @@ package com.java.designpattern.command;
 public class Main {
     
     public static void main(String[] args) {
-        // 这一步是启动之前就定好的
-        BoostCommand bc = new ABoostCommand();
-        StartButton sb = new StartButton();
-        sb.init(bc);
-        
-        // 点用户点击开始按钮就可以运行了
-        sb.start();
+        Client.assembleA(); // 组装电脑
+        Invoker invoker = Client.INVOKER;
+        // 开机
+        invoker.run();
     }
 }

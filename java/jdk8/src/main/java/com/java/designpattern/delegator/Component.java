f068745baf28d51cfa7b92fd1aa377f16ba7e219
zhangxq <zzzzail@163.com>
2022-09-19 11:32:30 +0800

    1

---
+++ java/jdk8/src/main/java/com/java/designpattern/delegator/Component.java
@@ -0,0 +1,12 @@
+package com.java.designpattern.delegator;
+
+/**
+ * @author zhangxq
+ * @since 2022/9/19
+ */
+public class Component {
+    
+    public void fun() {
+        System.out.println("Component::fun");
+    }
+}

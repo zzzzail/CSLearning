4d438341ea464e498dccf4fb518fd9b29782c1dc
zhangxq <zzzzail@163.com>
2022-09-20 09:37:19 +0800

    1

---
+++ java/jdk8/src/main/java/com/java/base/StaticInnerFun.java
@@ -0,0 +1,17 @@
+package com.java.base;
+
+/**
+ * @author zhangxq
+ * @since 2022/9/20
+ */
+public class StaticInnerFun {
+    
+    static {
+        // 类被加载的时候就执行
+        System.out.println("StaticInnerFun 的静态代码块执行");
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}

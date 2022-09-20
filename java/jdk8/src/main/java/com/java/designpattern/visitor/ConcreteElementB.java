a56f2640a1b3631506da41bc1b732698e945fdc5
zhangxq <zzzzail@163.com>
2022-09-20 13:59:49 +0800

    1

---
+++ java/jdk8/src/main/java/com/java/designpattern/visitor/ConcreteElementB.java
@@ -0,0 +1,19 @@
+package com.java.designpattern.visitor;
+
+/**
+ * @author zhangxq
+ * @since 2022/9/20
+ */
+public class ConcreteElementB extends Element {
+    
+    private String str;
+    
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visitConcreteElementB(this);
+    }
+    
+    public void operationB() {
+        System.out.println("ConcreteElementB::operationB");
+    }
+}

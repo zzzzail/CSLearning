abfa9200db63ff764c0fa6848d62cd998a87ac39
zhangxq <zzzzail@163.com>
2022-10-09 20:30:48 +0800

    1

--- java/jdk8/src/main/java/com/java/base/BigIntegerDemo.java
+++ java/jdk8/src/main/java/com/java/base/BigIntegerDemo.java
@@ -12,5 +12,7 @@ public class BigIntegerDemo {
         // radix 基数
         BigInteger num1 = new BigInteger("20", 10);
         System.out.println(num1);
+                        // 23697574004726811
+        System.out.println(11848287002363406L + 11849287002363405L);
     }
 }
--- java/jdk8/src/main/java/com/java/dsa/shijiwenti/PolygonUtilsTest.java
+++ java/jdk8/src/main/java/com/java/dsa/shijiwenti/PolygonUtilsTest.java
@@ -22,7 +22,7 @@ public class PolygonUtilsTest {
         System.out.println(PolygonUtils2.insidePolygon(p2, contour1));
         long[] p3 = {10, 11};
         System.out.println(PolygonUtils2.insidePolygon(p3, contour1));
-        
+
         // 地图上的三角形
         long[][] fence1 = new long[][]{
                 {1182446787, 391226895},
@@ -35,7 +35,7 @@ public class PolygonUtilsTest {
         System.out.println(insideRes1 ? "【正确】点在内部" : "点在外部");
         boolean outsideRes1 = PolygonUtils2.insidePolygon(outsidePoint1, fence1);
         System.out.println(outsideRes1 ? "点在内部" : "【正确】点在外部");
-        
+
         // 地图上的四边形
         long[][] fence2 = new long[][]{
                 {1183026014, 391488837},
@@ -49,32 +49,44 @@ public class PolygonUtilsTest {
         System.out.println(insideRes2 ? "【正确】点在内部" : "点在外部");
         boolean outsideRes2 = PolygonUtils2.insidePolygon(outsidePoint2, fence2);
         System.out.println(outsideRes2 ? "点在内部" : "【正确】点在外部");
-        
+
         // 围栏 1
         long[][] fence11 = new long[][]{
                 {11848287002363406L, 38956445319342585L},
                 {11849287002363405L, 38956445319342585L},
-                {11848287002363406L, 3896644531934258L}
+                {11848287002363406L, 38966445319342580L},
         };
         // 内部点
-        System.out.println(PolygonUtils2.insidePolygon(
-                new long[]{11848587002363406L, 3895944531934258L}, fence11));
+        long[] insideP4 =  {11848587002363406L, 38959445319342580L};
         //外部点
-        System.out.println(PolygonUtils2.insidePolygon(
-                new long[]{11848587002363406L, 3890944531934258L}, fence11));
-        
+        long[] outsideP4 = {11848587002363406L, 38909445319342580L};
+        System.out.println(PolygonUtils2.insidePolygon(insideP4, fence11));
+        System.out.println(PolygonUtils2.insidePolygon(outsideP4, fence11));
+
         // 围栏2
         long[][] fence22 = new long[][]{
                 {11848287002363406L, 38956445319342585L},
                 {11849287002363405L, 38956445319342585L},
-                {11849287002363405L, 3896644531934258L},
-                {11848287002363406L, 3896644531934258L}
+                {11849287002363405L, 38966445319342580L},
+                {11848287002363406L, 38966445319342580L},
         };
         // 内部点
-        System.out.println(PolygonUtils2.insidePolygon(
-                new long[]{11848587002363406L, 3895944531934258L}, fence22));
+        long[] insideP5 =  {11848832130432130L, 38957206297861840L};
         // 外部点
-        System.out.println(PolygonUtils2.insidePolygon(
-                new long[]{11848587002363406L, 3890944531934258L}, fence22));
+        long[] outsideP5 = {11848587002363406L, 38909445319342580L};
+        System.out.println(PolygonUtils2.insidePolygon(insideP5, fence22));
+        System.out.println(PolygonUtils2.insidePolygon(outsideP5, fence22));
+        
+        // 高精度的地图
+        long[][] polygon6 = new long[][]{
+                {11849398612976076L, 38994972934024110L},
+                {11850445747375490L, 38995239764368556L},
+                {11850248336791994L, 39002510503993660L},
+                {11849372863769533L, 39002043598210280L},
+        };
+        long[] insideP6 =  {11849836349487305L, 38997507781674905L};
+        long[] outsideP6 = {11853415489196779L, 39001376584603115L};
+        System.out.println(PolygonUtils2.insidePolygon(insideP6, polygon6));
+        System.out.println(PolygonUtils2.insidePolygon(outsideP6, polygon6));
     }
 }

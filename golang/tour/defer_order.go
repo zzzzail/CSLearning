44c1a8511f10bf8ce1532ae769aa6aa249c3b0fd
zhangxq <zzzzail@163.com>
2022-10-17 17:06:02 +0800

    1

--- golang/tour/defer_order.go
+++ golang/tour/defer_order.go
@@ -8,4 +8,16 @@ func main() {
 	defer fmt.Println("2")
 
 	fmt.Println("main running")
+
+	b, a := test(10)
+	fmt.Println(a, b)
+}
+
+func test(x int) (a, b int) {
+	defer fmt.Println("123")
+	defer fmt.Println("456")
+
+	fmt.Println("test running")
+
+	return 0, 1
 }

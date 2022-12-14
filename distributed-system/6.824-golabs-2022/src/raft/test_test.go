f33f20faf98ee7749e887fa32babb5cd3d5905ab
zhangxq <zzzzail@163.com>
2022-12-14 10:18:07 +0800

    1

--- distributed-system/6.824-golabs-2022/src/raft/test_test.go
+++ distributed-system/6.824-golabs-2022/src/raft/test_test.go
@@ -21,6 +21,7 @@ import (
 // (much more than the paper's range of timeouts).
 const RaftElectionTimeout = 1000 * time.Millisecond
 
+// 测试 初始化选举
 func TestInitialElection2A(t *testing.T) {
 	servers := 3
 	cfg := make_config(t, servers, false, false)
